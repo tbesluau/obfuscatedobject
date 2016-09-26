52ddd6ad263d92ed07c08c6cde4af72b16318713
Thomas Besluau <tbesluau@gmail.com>
2016-09-26 18:06:51 -0400

    adding getter and setter, and tests for them

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-module.export = {
+Object.prototype._getObfuscatedKey = function () {
+	var result = '';
+	while (!result || this.result !== undefined) {
+		result = Math.random().toString(36).substr(2, 12);
+	}
+	return result;
+};
 
+Object.prototype.oGet = function (key) {
+	if (typeof(key) !== 'string') {
+		if (typeof(key) === 'object' && key.key) {
+			key = key.key;
+		} else {
+			return;
+		}
+	}
+	var value = this[key];
+	var newKey = _getObfuscatedKey();
+	this[newKey] = value;
+	delete this[key];
+	return {key: newKey, value: value};
+};
+
+Object.prototype.oSet = function (key, value) {
+	if (typeof(key) !== 'string') {
+		if (typeof(key) === 'object' && key.key) {
+			value = key.value;
+			key = key.key;
+		} else {
+			return;
+		}
+	}
+	var newKey = _getObfuscatedKey();
+	this[newKey] = value;
+	delete this[key];
+	return {key: newKey, value: value};
 };
--- index.mocha.js
+++ index.mocha.js
@@ -1,8 +1,73 @@
 var assert = require('assert');
-var obfuscatedobject = require('./index.js');
+require('./index.js');
 
 describe('Import', function() {
-    it('should be an object', function() {
-      assert.equal(typeof(obfuscatedobject), 'object');
+    it('prototype should be overridden', function() {
+		assert.equal(typeof({}.oGet), 'function');
+		assert.equal(typeof({}.oSet), 'function');
+    });
+});
+
+describe('Get Key', function() {
+    it('method exists and returns 12 char keys', function() {
+		assert.equal(typeof({}._getObfuscatedKey), 'function');
+		assert.equal({}._getObfuscatedKey().length, 12);
+    });
+});
+
+describe('Set', function() {
+    it('setter actually puts it there', function() {
+		var obj = {};
+		var key = obj.oSet('', 123).key;
+		assert.equal(obj[key], 123);
+    });
+	it('setter updates properly', function() {
+		var obj = {};
+		var key = obj.oSet('', 123).key;
+		assert.equal(obj[key], 123);
+		var newKey = obj.oSet(key, 321).key;
+		assert.equal(obj[newKey], 321);
+		assert.equal(obj[key], undefined);
+    });
+});
+
+describe('Get', function() {
+    it('getter retrieves properly', function() {
+		var obj = {};
+		var key = obj.oSet('', 123).key;
+		assert.equal(obj.oGet(key).value, 123);
+    });
+	it('getter retrieves properly twice', function() {
+		var obj = {};
+		var key = obj.oSet('', 123).key;
+		var oobj = obj.oGet(key);
+		assert.equal(oobj.value, 123);
+		var newKey = obj.oSet(oobj.key, 321).key;
+		assert.equal(obj[newKey], 321);
+		assert.equal(obj[key], undefined);
+    });
+	it('empty key returns undefined', function() {
+		var obj = {};
+		var key = obj.oSet('', 123).key;
+		assert.equal(obj.oGet('foo').value, undefined);
+    });
+});
+
+describe('Hybrid', function() {
+    it('setter updates properly', function() {
+		var obj = {
+			foo: 'bar'
+		};
+		var key = obj.oSet('foo', 123).key;
+		assert.equal(obj.oGet(key).value, 123);
+		assert.equal(obj.foo, undefined);
+    });
+	it('getter retrieves and obfuscates', function() {
+		var obj = {
+			foo: 'bar'
+		};
+		var oobj = obj.oGet('foo');
+		assert.equal(oobj.value, 'bar');
+		assert.equal(obj.foo, undefined);
     });
 });
